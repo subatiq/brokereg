4b8c627750b3aecce5355146387d975390e776c3
Vladimir Semenov <sepsseven@gmail.com>
2022-10-18 18:02:33 +0300

    Initial commit

---
+++ brokereg/event.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+from pydantic import BaseModel, Field
+
+
+class Event(BaseModel):
+    event_id: UUID = Field(default_factory=uuid4)
+    event_version: int = 1
+    event_name: str
+    event_producer: str
+    event_time: datetime = Field(default_factory=datetime.utcnow)
+    content: BaseModel
+
